f860ed09ec0daa31539a2310bdd1165f8b24076a
Prabhat Dutt <rajprabhat903@gmail.com>
2023-02-25 01:27:34 +0530

    Added New Phones section

--- index.js
+++ index.js
@@ -17,34 +17,48 @@ $(document).ready(function () {
   //top_sale owl carousel
   $("#top-sale .owl-carousel").owlCarousel({
     loop: true,
-    nav: true,
+    dots: true,
     autoplay: true,
-    dots: false,
     responsive: {
       0: {
         items: 1,
-        nav: true,
       },
       600: {
         items: 3,
-        nav: true,
       },
       1000: {
         items: 6,
-        nav: true,
-      }
-    }    
+      },
+    },
   });
 
   //Isotope Filter
   var $grid = $(".grid").isotope({
-    itemSelector:'.grid-item',
-    layoutMode:'fitRows'
+    itemSelector: ".grid-item",
+    layoutMode: "fitRows",
   });
 
   //filter items on button click
-  $(".button-group").on("click", "button",function(){
-    var filterValue = $(this).attr('data-filter');
-    $grid.isotope({filter:filterValue});
-  })
+  $(".button-group").on("click", "button", function () {
+    var filterValue = $(this).attr("data-filter");
+    $grid.isotope({ filter: filterValue });
+  });
+
+  //new_phones owl carousel
+  $("#new_phones .owl-carousel").owlCarousel({
+    loop: true,
+    dots: true,
+    autoplay: true,
+    responsive: {
+      0: {
+        items: 1,
+      },
+      600: {
+        items: 3,
+      },
+      1000: {
+        items: 6,
+      },
+    },
+  });
 });
