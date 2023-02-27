2bf9ef69e0ffbbc06186216de84fa21071b0435d
Prabhat Dutt <rajprabhat903@gmail.com>
2023-02-28 02:39:58 +0530

    added cart.html

--- index.js
+++ index.js
@@ -82,24 +82,23 @@ $(document).ready(function () {
     },
   });
 
-  //product qty [quantity] section (of product.html)
-  let qty_up = $(".qty .qty-up");
-  let qty_down = $(".qty .qty-down");
-  let qty_input = $(".qty .qty-input");
-
   //click on qty up button
-  qty_up.click(function (e) {
-    if (qty_input.val() >= 1 && qty_input.val() <= 9) {
-      qty_input.val(function (i, oldval) {
+  $(".qty").on("click", ".qty-up", function (e) {
+    let parent = $(this).closest(".qty");
+    let input = parent.find(".qty-input");
+    if (input.val() >= 1 && input.val() <= 9) {
+      input.val(function (i, oldval) {
         return ++oldval;
       });
     }
   });
 
   // click on qty down button
-  qty_down.click(function (e) {
-    if (qty_input.val() > 1 && qty_input.val() <= 10) {
-      qty_input.val(function (i, oldVal) {
+  $(".qty").on("click", ".qty-down", function (e) {
+    let parent = $(this).closest(".qty");
+    let input = parent.find(".qty-input");
+    if (input.val() > 1 && input.val() <= 10) {
+      input.val(function (i, oldVal) {
         return --oldVal;
       });
     }
